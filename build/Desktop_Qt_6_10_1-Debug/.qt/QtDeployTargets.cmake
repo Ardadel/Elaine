2adf5153cb2908cf212440a97275b99cfc797a16
Argonauta <igor.puscion@gmail.com>
2025-12-19 22:57:03 +0100

    Inicjalizacja projektu

---
+++ build/Desktop_Qt_6_10_1-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appElaine_FILE /home/argo/Documents/qt/Elaine/build/Desktop_Qt_6_10_1-Debug/appElaine)
+set(__QT_DEPLOY_TARGET_appElaine_TYPE EXECUTABLE)
---
+++ build/Desktop_Qt_6_10_1-Debug/.qt/appElaine_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appElaine")
+set(working_dir "/home/argo/Documents/qt/Elaine")
+set(src_and_dest_list
+    "/home/argo/Documents/qt/Elaine/Main.qml"
+    "/home/argo/Documents/qt/Elaine/build/Desktop_Qt_6_10_1-Debug/Elaine/Main.qml"
+
+)
+set(timestamp_file "/home/argo/Documents/qt/Elaine/build/Desktop_Qt_6_10_1-Debug/.qt/appElaine_qml.txt")
